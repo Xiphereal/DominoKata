203a7887962743d71b49d0130933dfb963e6f8d7
Xiphereal <lozanojalex@gmail.com>
2025-01-10 15:46:40 +0100

    docs: remove already implemented tests from list

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -7,11 +7,6 @@ namespace DominoKata.Tests;
 public class Tests
 {
     // Circular chain not possible -> Output it.
-    // Ask if set is in circle
-    //  - [1|1] is
-    //  - [1|2] is not
-    //  - [1|2] [2|1] is
-    //  - [1|2] [1|2] is not
     // Order set.
     //  - [1|2] [2|1] -> [1|2] [2|1]
     //  - [1|2] [1|2] -> [1|2] [2|1]
