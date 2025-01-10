2419a1897401ba19ea26f4af7ddb2fb41cbced79
Xiphereal <lozanojalex@gmail.com>
2025-01-10 15:14:46 +0100

    chore: create empty project

---
+++ Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using DominoKata;
+using NUnit.Framework;
+
+namespace DominoKata.Tests;
+
+public class Tests
+{
+    [Test]
+    public void METHOD()
+    {
+        Assert.Fail("asdfsadf");
+    }
+}
