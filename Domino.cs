a2e6a3e27ebc044c6253d1949dd6da3b53302ee6
Xiphereal <lozanojalex@gmail.com>
2025-01-10 15:48:47 +0100

    refactor: preconditioned that Dominos dots are [1-6]

--- Domino.cs
+++ Domino.cs
@@ -7,6 +7,12 @@ public class Domino
 
     public Domino(int left, int right)
     {
+        if (left is <= 0 or > 6)
+            throw new ArgumentException();
+        
+        if (right is <= 0 or > 6)
+            throw new ArgumentException();
+        
         this.left = left;
         this.right = right;
     }
